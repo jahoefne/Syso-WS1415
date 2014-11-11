ecbf52854ac54c690fcc2127eac03c6742c2a4a4
Jan-Moritz Hoefner <jahoefne@htwg-konstanz.de>
2014-11-11 08:17:30 +0100

    busybox compile, for arm

--- V2/script.sh
+++ V2/script.sh
@@ -7,11 +7,16 @@
 cp ./config.config ../../linux-3.17.1/
 mv ../../linux-3.17.1/config.config ../../linux-3.17.1/.config
 
+# compile busybox for arm
+cp busybox.config ../../busybox-1.22.1/.config
+cd ../../busybox-1.22.1
+ARCH=arm CROSS_COMPILE=/opt/toolchains/arm-buildroot-linux-uclibcgnueabihf-4.9.1/bin/arm-buildroot-linux-uclibcgnueabihf- make
+cp ./busybox ../syso/V2/systemx86/bin
 
-cd ../../linux-3.17.1
+cd ../linux-3.17.1
 #make clean
 ARCH=arm CROSS_COMPILE=/opt/toolchains/arm-buildroot-linux-uclibcgnueabihf-4.9.1/bin/arm-buildroot-linux-uclibcgnueabihf- make -j 4
-qemu-system-arm -kernel arch/arm/boot/zImage -machine vexpress-a9
+qemu-system-arm -kernel arch/arm/boot/zImage -machine vexpress-a9 -append "console=tty1"
 
 
 
